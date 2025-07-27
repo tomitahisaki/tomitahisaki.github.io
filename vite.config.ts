a68d18c56e104598caf087509d300bf44dd30c6b
Hisakey <hogehoge@example.com>
2025-07-27 14:38:18 +0900

    feat: base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? '/tomitahisaki.github.io' : '/',
+  base: '/tomitahisaki.github.io/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
